9ec2de6588a491b2d866b1bed32d973e19bfaa4a
AngelEsc-2021 <aescobarv1@miumg.edu.gt>
2023-10-19 13:32:38 -0600

    Login

---
+++ app/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Login from './assets/Componentes/Login/login'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Login />
+  </React.StrictMode>,
+)
